a54acbf7cf931023f46ec7f7e4779519bfc6f7fb
zhougl <991314742@qq.com>
2017-11-09 16:47:47 +0800

    新加许愿工程
    新加

---
+++ StartActivityForInputMessage/app/src/main/java/com/jiekexueyuan/startactivityforinputmessage/MainActivity.java
@@ -0,0 +1,32 @@
+package com.jiekexueyuan.startactivityforinputmessage;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private TextView mTvObtainContent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent i = getIntent();
+        mTvObtainContent = (TextView) findViewById(R.id.tv1);
+        mTvObtainContent.setText(i.getStringExtra("data"));
+
+        findViewById(R.id.btn1).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent messageInput = new Intent(MainActivity.this,MessageInputActivity.class);
+        startActivity(messageInput);
+    }
+}
